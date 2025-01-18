c983df3037a70a8e2e263422008ba9aa72779386
Shri Ram dwivedi <raunakd511@gmail.com>
2025-01-18 14:31:19 +0530

    Equilateral Triangle

---
+++ EquilateralTrainagle.py
@@ -0,0 +1,10 @@
+num = int(input("Enter Any Number : "))
+
+for x in range(num):     # Row
+    for y in range(num-x):    #Coloumn
+        print(" ",end="")
+    for z in range(x+1):
+        print("*",end="")
+    for v in range(x):
+        print("*",end="")
+    print()
