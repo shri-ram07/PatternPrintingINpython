b32c98df00941f274ac78e2cf84bb0b4d952f452
Shri Ram dwivedi <raunakd511@gmail.com>
2025-01-18 14:42:43 +0530

    Adding Diamond Pattern

---
+++ Diamond.py
@@ -0,0 +1,17 @@
+num = int(input("Enter Any Number : "))
+for x in range(num-1):     # Row
+    for y in range(num-x):    #Coloumn
+        print(" ",end="")
+    for z in range(x+1):
+        print("*",end="")
+    for v in range(x):
+        print("*",end="")
+    print()
+for x in range(num):     # Row
+    for y in range(x+1):    #Coloumn
+        print(" ",end="")
+    for z in range(num-x):
+        print("*",end="")
+    for v in range(num-x-1):
+        print("*",end="")
+    print()
